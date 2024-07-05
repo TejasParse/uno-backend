e790ca49ae7287adef2f5f1ba22827a7d357b3f8
Tejas <tejasparse1@gmail.com>
2024-07-06 02:36:48 +0530

    Reset and Game Working

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-const { filterGameData, updateAllPlayers, getNewDecks } = require("./utils/helper")
+const { filterGameData, updateAllPlayers, getNewDecks, getNextTurn, getRandomCards } = require("./utils/helper")
 
 const { join_room, host_message_send, player_message_send } = require("./controllers/socketControllers");
-const cards = require("./assets/cards.json")
+const cards = require("./assets/cards.json");
+const e = require("express");
 
 app.use(cors());
 const server = http.createServer(app);
@@ -32,7 +33,7 @@ const io = new Server(server, {
 //   direction: 0,
 //   winners: [],
 
-//   messages: []
+//   messages: [{  }]
 // }
 let gameRooms = [];
 
@@ -45,27 +46,42 @@ let gameRooms = [];
 let onlineUsers = [];
 const removeUsers = (userdata, index, socketClient) => {
 
-	let gameIndex = gameRooms.findIndex((elm)=> elm.roomNo === userdata.roomNo);
-	if(gameIndex !== -1) {
-		
-		let unHostMembers = gameRooms[gameIndex].players.filter((elm)=> !elm.isHost)
 
-		if(unHostMembers.length === 0) {
+
+	let gameIndex = gameRooms.findIndex((elm) => elm.roomNo === userdata.roomNo);
+	if (gameIndex !== -1) {
+
+		gameRooms[gameIndex].messages.push({
+			sender: "HOST",
+			message: `${userdata.username} disconnected!`,
+			type: "command"
+		})
+
+		let unHostMembers = gameRooms[gameIndex].players.filter((elm) => !elm.isHost)
+
+		if (unHostMembers.length === 0) {
 			gameRooms.splice(gameIndex, 1);
 			console.log(userdata.roomNo, "Removing this room from array");
 		} else {
 			// TODO: Set a new host
 			console.log("Will Set a new host");
-			gameRooms[gameIndex].players = gameRooms[gameIndex].players.filter((elm)=> elm.username!== userdata.username)
+			gameRooms[gameIndex].players = gameRooms[gameIndex].players.filter((elm) => elm.username !== userdata.username)
 
-			if(userdata.isHost) {
+			if (userdata.isHost) {
 				let hostUser = unHostMembers[0];
-				let hostIndex = gameRooms[gameIndex].players.findIndex((elm)=> elm.socketId === hostUser.socketId)
+				let hostIndex = gameRooms[gameIndex].players.findIndex((elm) => elm.socketId === hostUser.socketId)
 				socketClient.to(hostUser.socketId).emit("set_host", {
-	
+
 				})
 				gameRooms[gameIndex].players[hostIndex].isHost = 1;
+				gameRooms[gameIndex].messages.push({
+					sender: "HOST",
+					message: `${hostUser.username} is the new host!`,
+					type: "command"
+				})
 			}
+
+
 
 
 
@@ -77,6 +93,7 @@ const removeUsers = (userdata, index, socketClient) => {
 	onlineUsers.splice(index, 1);
 
 };
+
 
 
 
@@ -246,7 +263,7 @@ io.on("connection", (socket) => {
 			} while (gameRooms[index].selectedIndexes.has(randomIndex));
 
 
-			
+
 			gameRooms[index].players = players;
 			gameRooms[index].selectedIndexes = selectedIndexes;
 			gameRooms[index].selectedIndexes.add(randomIndex);
@@ -306,12 +323,185 @@ io.on("connection", (socket) => {
 				}
 
 			}
-			
+
 			gameRooms[index].current_turn = newCurrentTurn1
 
 
 			updateAllPlayers(socket, gameRooms[index])
 		}
+	})
+
+	socket.on("play_card", (data) => {
+		const userDetails = onlineUsers.find((elm) => elm.socketId === socket.id)
+
+		console.log(userDetails, "This user played", data.cardNo);
+
+		if (userDetails) {
+			let gameIndex = gameRooms.findIndex((elm) => elm.roomNo === userDetails.roomNo);
+			if (gameIndex !== -1) {
+
+				const newPlayedCard = cards[data.cardNo]
+
+				let playerIndex = gameRooms[gameIndex].players.findIndex((elm) => elm.username === userDetails.username)
+				let player = gameRooms[gameIndex].players[playerIndex]
+
+				const newSelectedIndexes = gameRooms[gameIndex].selectedIndexes;
+				newSelectedIndexes.delete(data.cardNo);
+
+				const newPlayerCards = player.cards.filter(card => card !== data.cardNo);
+
+				let dir = gameRooms[gameIndex].direction
+				let oldDir = dir;
+				if (newPlayedCard.display_text === "Reverse") {
+					dir ^= 1
+					console.log("Reversing the order", dir);
+				}
+
+				let newCurrentTurn = getNextTurn(gameRooms[gameIndex].players, dir, gameRooms[gameIndex].current_turn)
+				let nextTurn = newCurrentTurn
+
+
+				let newState = {
+					...gameRooms[gameIndex],
+					presentCard: data.cardNo,
+					selectedIndexes: newSelectedIndexes,
+					players: [
+						...gameRooms[gameIndex].players.slice(0, playerIndex),
+						{
+							...player,
+							cards: newPlayerCards
+						},
+						...gameRooms[gameIndex].players.slice(playerIndex + 1)
+					],
+					current_turn: newCurrentTurn,
+					direction: dir,
+					messages: [
+						...gameRooms[gameIndex].messages,
+						{
+							sender: player.username,
+							message: `${player.username} played ${newPlayedCard.card_name}`,
+							type: "command"
+						}
+					]
+				};
+
+
+
+
+				if (newPlayedCard.display_text === "+2") {
+					let { selectedCards, selectedIndexes: newIndexs } = getRandomCards(gameRooms[gameIndex].selectedIndexes, 2)
+					console.log(selectedCards, "Draw Two Cards", nextTurn);
+
+					newState = {
+						...newState,
+						selectedIndexes: newIndexs,
+						players: [
+							...newState.players.slice(0, nextTurn),
+							{
+								...newState.players[nextTurn],
+								cards: [
+									...newState.players[nextTurn].cards,
+									...selectedCards
+								]
+							},
+							...newState.players.slice(nextTurn + 1)
+						]
+					};
+					console.log(newState, "New State?", newState?.players[nextTurn], "New PLayer", nextTurn);
+				}
+
+				if (newPlayerCards.length === 0) {
+					console.log("Game Over");
+					newState = {
+						...gameRooms[gameIndex],
+						presentCard: data.cardNo,
+						selectedIndexes: newSelectedIndexes,
+						players: [
+							...gameRooms[gameIndex].players.slice(0, playerIndex),
+							...gameRooms[gameIndex].players.slice(playerIndex + 1)
+						],
+						current_turn: newState.current_turn,
+						direction: dir,
+						winners: [
+							...gameRooms[gameIndex].winners,
+							{
+								...player,
+								cards: newPlayerCards
+							},
+						]
+					};
+					console.log(newState, "Player Won. Changing State");
+					if (oldDir === 0) {
+
+						if (newCurrentTurn !== 0) {
+							newState.current_turn = newCurrentTurn - 1;
+						}
+
+					} else if (oldDir === 1) {
+
+						if (newCurrentTurn !== (newState.players.length - 1)) {
+							newState.current_turn = newCurrentTurn + 1;
+						}
+
+					}
+				}
+
+				if (newPlayedCard.display_text === "Skip") {
+					newState.current_turn = getNextTurn(newState.players, newState.direction, newState.current_turn);
+				}
+				gameRooms[gameIndex] = newState;
+
+				updateAllPlayers(socket, gameRooms[gameIndex])
+			}
+		}
+	})
+
+	socket.on("reset_game", (data) => {
+
+		const hostDetails = onlineUsers.find((elm)=>elm.socketId === socket.id)
+
+		let gameIndex = gameRooms.findIndex((elm) => elm.roomNo === data.roomNo);
+		if(gameIndex !== -1) {
+
+			let newPlayers = [
+				...gameRooms[gameIndex].players,
+				...gameRooms[gameIndex].winners
+			]
+			const freshIndexes = new Set()
+			const { players: players1, selectedIndexes: newIndexes } = getNewDecks(newPlayers, freshIndexes)
+	
+			// console.log(selectedIndexes, "What is happenig");
+	
+			let randomIndex12;
+			do {
+				randomIndex12 = Math.floor(Math.random() * cards.length);
+			} while (newIndexes.has(randomIndex12));
+	
+	
+			newIndexes.add(randomIndex12);
+	
+			let new_state123 = {
+				...gameRooms[gameIndex],
+				players: players1,
+				presentCard: randomIndex12,
+				selectedIndexes: newIndexes,
+				started: 1,
+				current_turn: 0,
+				direction: 0,
+				winners: [],
+				messages: [
+					...gameRooms[gameIndex].messages,
+					{
+						sender: hostDetails?.username || "HOST",
+						message: `${hostDetails?.username || "HOST"} initiated a game reset`,
+						type: "command"
+					}
+				]
+			};
+	
+			updateAllPlayers(socket, new_state123)
+		}
+
 	})
 
 	socket.on("host_message_send", (data) => {
@@ -323,9 +513,9 @@ io.on("connection", (socket) => {
 	})
 
 	socket.on("disconnect", () => {
-		const userIndex = onlineUsers.findIndex((elm)=> elm.socketId === socket.id)
+		const userIndex = onlineUsers.findIndex((elm) => elm.socketId === socket.id)
 		console.log(socket.id, "Someone disconnected?", userIndex, onlineUsers);
-		if(userIndex!== -1) {
+		if (userIndex !== -1) {
 
 			// console.log(onlineUsers[userIndex], "Removed From Online Users");
 			removeUsers(onlineUsers[userIndex], userIndex, socket)
--- utils/helper.js
+++ utils/helper.js
@@ -29,8 +29,13 @@ exports.filterGameData = (gameData) => {
 
 exports.updateAllPlayers = (socketClient, gameData) => {
 
-    for (let i = 0; i < gameData.players.length; i++) {
-        let playerInfo = gameData.players[i];
+    let allPlayers = [
+        ...gameData.players,
+        // ...gameData.winners
+    ]
+
+    for (let i = 0; i < allPlayers.length; i++) {
+        let playerInfo = allPlayers[i];
 
         let payload = {
             game: this.filterGameData(gameData),
@@ -61,7 +66,7 @@ exports.getNewDecks = (players, selectedIndexes) => {
 
     for (let i = 0; i < numberOfSets; i++) {
         const selectedCards = [];
-        while (selectedCards.length < 10) {
+        while (selectedCards.length < 2) {
             const randomIndex = Math.floor(Math.random() * cards.length);
             if (!selectedIndexes.has(randomIndex)) {
                 selectedIndexes.add(randomIndex);
@@ -72,4 +77,39 @@ exports.getNewDecks = (players, selectedIndexes) => {
         selectedSets.push(selectedCards);
     }
     return { players, selectedIndexes }
+}
+
+exports.getNextTurn = (players, dir, current_turn) => {
+	let newCurrentTurn
+	if (!dir) {
+		newCurrentTurn = current_turn + 1;
+		if (newCurrentTurn >= players.length) {
+			newCurrentTurn = 0;
+		}
+
+	} else {
+
+		newCurrentTurn = current_turn - 1;
+		if (newCurrentTurn < 0) {
+			newCurrentTurn = players.length - 1;
+		}
+	}
+
+	return newCurrentTurn
+
+}
+
+exports.getRandomCards = (selectedIndexes, cardsCount) => {
+	if (selectedIndexes) {
+		const selectedCards = [];
+		while (selectedCards.length < cardsCount) {
+			const randomIndex = Math.floor(Math.random() * cards.length);
+			if (!selectedIndexes?.has(randomIndex)) {
+				selectedIndexes?.add(randomIndex);
+				selectedCards.push(randomIndex);
+			}
+		}
+
+		return { selectedCards, selectedIndexes }
+	}
 }
